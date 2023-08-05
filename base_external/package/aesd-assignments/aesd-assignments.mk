a3a16fbf04856d5e68f1a9d069a65f599b4078a1
Aaron <aaron@Ubuntu.myguest.virtualbox.org>
2023-08-05 15:17:22 -0700

    make-file-mod-for-assgiment-3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,8 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = 'c6fcc6638a030f3dcaf056a020e57272f7a584f7'
-AESD_ASSIGNMENTS_VERSION = 'e7c17d2'
+#AESD_ASSIGNMENTS_VERSION = 'e7c17d2'
+AESD_ASSIGNMENTS_VERSION = 'f1b41c2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -24,6 +25,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh  $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
