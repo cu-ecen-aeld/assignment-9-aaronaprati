f58927b9e7bef912f740d5d230f9fe77efa02a45
Aaron <aaron@Ubuntu.myguest.virtualbox.org>
2023-10-01 00:36:17 -0700

    modification

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -7,7 +7,8 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 
 #AESD_CHAR_VERSION = 86905b31041ad5f2de718f120369d0b8d374eb5e
-AESD_CHAR_VERSION =796a9360d5a7e9f0e31855c9d14cb887426d9cc7
+#AESD_CHAR_VERSION =796a9360d5a7e9f0e31855c9d14cb887426d9cc7
+AESD_CHAR_VERSION = f8be96dc5c3e6e2364b242763aeba6d97012ae92
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
